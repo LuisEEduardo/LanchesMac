1724871b6d972d8e052ae7ab0290cd2e6aa46e4d
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-06-16 18:36:08 -0300

    Criando migration

--- LanchesMac/Models/Categoria.cs
+++ LanchesMac/Models/Categoria.cs
@@ -1,9 +1,22 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
 {
+    [Table("Categoria")]
     public class Categoria
     {
+        [Key]
         public int Id { get; private set; }
+
+        [StringLength(100, ErrorMessage = "O tamanho máximo e de 100 carateres")]
+        [Required(ErrorMessage = "Informe o nome da categoria")]        
+        [Display(Name = "Nome")]
         public string Nome { get; private set; }
+
+        [StringLength(200, ErrorMessage = "O tamanho máximo e de 100 carateres")]
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [Display(Name = "Descrição")]
         public string Descricao { get; private set; }
 
         public List<Lanche> Lanches { get; set; }
--- LanchesMac/Models/Lanche.cs
+++ LanchesMac/Models/Lanche.cs
@@ -11,6 +11,7 @@ namespace LanchesMac.Models
         
         [Required(ErrorMessage = "O nome do lanche deve ser informado")]
         [Display(Name = "Nome do lanche")]
+        [StringLength(maximumLength: 80, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2}")]
         public string Nome { get; private set; }
 
         [Required(ErrorMessage = "A descrição do lanche deve ser informada")]
@@ -18,11 +19,31 @@ namespace LanchesMac.Models
         [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
         [MaxLength(200, ErrorMessage = "Descrição não pode exceder {1} carateres")]
         public string DescricaoCurta { get; private set; }
+
+        [Required(ErrorMessage = "A descrição detalhada deve ser informada")]
+        [Display(Name = "Descrição detalhada do lanche")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição não pode exceder {1} carateres")]
         public string DescricaoDetalhada { get; private set; }
+
+        [Required(ErrorMessage = "Informe o preço do lanche")]
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(1, 999.99, ErrorMessage = "O preço deve estar entre 1 e 999,99")]
         public decimal Preco { get; private set; }
+
+        [Display(Name = "Caminho da imagem normal")]
+        [StringLength(200, ErrorMessage = ") {0} deve ter no máximo {1} caracteres")]
         public string ImagemUrl { get; private set; }
+
+        [Display(Name = "Caminho da imagem miniatura")]
+        [StringLength(200, ErrorMessage = ") {0} deve ter no máximo {1} caracteres")]
         public string ImagemThumbnailUrl { get; private set; }
+
+        [Display(Name = "Preferido?")]
         public bool IsLanchePreferido { get; private set; }
+
+        [Display(Name = "Estoque")]
         public bool EmEstoque { get; private set; }
 
         public int CategoriaId { get; set; }
