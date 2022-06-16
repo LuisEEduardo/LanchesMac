679c88e1ff05329eed95cafe84ae7a97c8833707
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-06-16 15:46:53 -0300

    Usando data annotations

--- LanchesMac/Models/Lanche.cs
+++ LanchesMac/Models/Lanche.cs
@@ -1,9 +1,22 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
 {
+    [Table("Lanches")]
     public class Lanche
-    {
+    {        
+        [Key]
         public int Id { get; private set; }
+        
+        [Required(ErrorMessage = "O nome do lanche deve ser informado")]
+        [Display(Name = "Nome do lanche")]
         public string Nome { get; private set; }
+
+        [Required(ErrorMessage = "A descrição do lanche deve ser informada")]
+        [Display(Name = "Descrição do lanche")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição não pode exceder {1} carateres")]
         public string DescricaoCurta { get; private set; }
         public string DescricaoDetalhada { get; private set; }
         public decimal Preco { get; private set; }
