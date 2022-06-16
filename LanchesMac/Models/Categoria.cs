8254af6a0d642dcebe2cb94259b339af7c72d65b
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-06-16 15:27:16 -0300

    Projeto LancheMac

---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+
+        public List<Lanche> Lanches { get; set; }
+    }
+}
---
+++ LanchesMac/Models/Lanche.cs
@@ -0,0 +1,18 @@
+﻿namespace LanchesMac.Models
+{
+    public class Lanche
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string DescricaoCurta { get; private set; }
+        public string DescricaoDetalhada { get; private set; }
+        public decimal Preco { get; private set; }
+        public string ImagemUrl { get; private set; }
+        public string ImagemThumbnailUrl { get; private set; }
+        public bool IsLanchePreferido { get; private set; }
+        public bool EmEstoque { get; private set; }
+
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
