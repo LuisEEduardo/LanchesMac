594efd339914b90176aa9a0ee806acb5fd6bbaff
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-06-16 21:32:12 -0300

    Criando a listagem dos lanches

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepositorio _lancheRepositorio;
+
+        public LancheController(ILancheRepositorio lancheRepositorio)
+        {
+            _lancheRepositorio = lancheRepositorio;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepositorio.Lanches;
+            return View(lanches);
+        }
+    }
+}
