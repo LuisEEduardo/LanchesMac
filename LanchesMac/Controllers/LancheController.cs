7ea26cff2856baf133300623f41ebc0a77f46889
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-06-16 22:48:47 -0300

    Usando ViewData, ViewBag e TempData

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -14,7 +14,13 @@ namespace LanchesMac.Controllers
 
         public IActionResult List()
         {
+            ViewData["Titulo"] = "Todos os Lanches";
+            ViewData["Data"] = DateTime.Now;
             var lanches = _lancheRepositorio.Lanches;
+
+            var totalLanches = lanches.Count();
+            ViewBag.Total = $"Total de lanches: {totalLanches}";
+
             return View(lanches);
         }
     }
