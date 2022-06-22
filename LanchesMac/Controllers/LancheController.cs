c049710513f3a97318efba02e44e4592d3a34468
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-06-22 20:42:45 -0300

    Adicionando atualização

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
-﻿using LanchesMac.Repositories.Interfaces;
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
 using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,12 +14,41 @@ namespace LanchesMac.Controllers
             _lancheRepositorio = lancheRepositorio;
         }
 
-        public IActionResult List()
+        public IActionResult List(string categoria)
         {
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = string.Empty;
+
+            if (string.IsNullOrEmpty(categoria))
+            {
+                lanches = _lancheRepositorio
+                            .Lanches
+                            .OrderBy(l => l.Id);
+                categoriaAtual = "Todos os lanches";
+            }
+            else
+            {
+                if (string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))
+                {
+                    lanches = _lancheRepositorio
+                                .Lanches
+                                .Where(l => l.Categoria.Nome.Equals("Normal"))
+                                .OrderBy(l => l.Nome);
+                }
+                else
+                {
+                    lanches = _lancheRepositorio
+                                .Lanches
+                                .Where(l => l.Categoria.Nome.Equals("Natural"))
+                                .OrderBy(l => l.Nome);
+                }
+                categoriaAtual = categoria;
+            }
+
             var lanchesListViewModel = new LancheListViewModel
             {
-                Lanches = _lancheRepositorio.Lanches,
-                CategoriaAtual = "Categoria atual"
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual
             };
 
             return View(lanchesListViewModel);
